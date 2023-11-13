f38b425fffd88db3f6fbb41d87b32283c68dcaeb
husseinkamal <hussain@aqwas.sa>
2023-11-13 21:57:12 +0200

    Instagram Profile UI Design

---
+++ app/src/main/java/com/hussein/instagramprofileui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.hussein.instagramprofileui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hussein.instagramprofileui.ui.theme.InstagramProfileUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InstagramProfileUITheme {
+    }
+}
